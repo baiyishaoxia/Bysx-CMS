495938914e2d1fb2517448766a8487324637f13b
baiyishaoxia <34636159+baiyishaoxia@users.noreply.github.com>
2018-07-15 20:50:26 +0800

    common config
    公共配置

---
+++ common/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2mk',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true, //邮件发送配置，true表示生成邮件在本地，false表示真实发送
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.126.com',
+                'username' => 'tzf2273465837@126.com',
+                'password' => 'tzf2273465837',//如果是126邮箱，此处请填写授权码
+                'port' => '25',
+                'encryption' => 'tls',
+            ],
+        ],
+    ],
+];
