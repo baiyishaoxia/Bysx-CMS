274241bf4d93bab3be3e829dfe6ab263815310a5
baiyishaoxia <34636159+baiyishaoxia@users.noreply.github.com>
2018-12-09 20:24:12 +0800

    by update
    入口文件

---
+++ frontend/web/index.php
@@ -0,0 +1,23 @@
+<?php
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_ENV_TEST') or define('YII_ENV_TEST',false);
+
+require(__DIR__ . '/../../vendor/autoload.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/bootstrap.php');
+require(__DIR__ . '/../config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/main.php'),
+    require(__DIR__ . '/../../common/config/main-local.php'),
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main-local.php')
+);
+
+$application = new yii\web\Application($config);
+//中英文切换配置
+$application->language = isset($_COOKIE['language']) ? htmlspecialchars($_COOKIE['language']) : 'zh-CN';
+//设置时区
+date_default_timezone_set('PRC');
+$application->run();
